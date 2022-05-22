7c8a0cbeff28ee8c44217867d622d3ffa544f929
Akif Tunç <akiftuncc@outlook.com>
2022-05-22 20:39:54 +0300

    xx
    xx

--- new.py
+++ new.py
@@ -1,1 +1,38 @@
-saddsadsa
+langTXT = open("deneme.txt","r")
+ 
+
+
+a = ["topla"]
+
+
+x = input("")
+
+if x == a[0]:
+    print(10+15)
+
+
+
+
+
+
+
+lang_list = []
+for i in langTXT:
+    lang_list.append(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
